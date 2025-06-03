2ce1cf5b637c70ab45dcda7c62e6f60c1212019a
TuanDatCoder <tuandatdq03@gmail.com>
2025-06-03 15:55:10 +0700

    Add CRUD ProjectPosition

---
+++ IntelliPM.Data/DTOs/ProjectPosition/Response/ProjectPositionResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Data.DTOs.ProjectPosition.Response
+{
+    public class ProjectPositionResponseDTO
+    {
+        public int Id { get; set; }
+        public int ProjectMemberId { get; set; }
+        public string Position { get; set; } = null!;
+        public DateTime AssignedAt { get; set; }
+    }
+}
