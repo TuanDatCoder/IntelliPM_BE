2f3cd652493907ecd8fa13bee64e44ef3888eaac
TuanDatCoder <tuandatdq03@gmail.com>
2025-06-16 18:37:03 +0700

    fix before commit 2d600209

--- IntelliPM.API/Program.cs
+++ IntelliPM.API/Program.cs
@@ -1,6 +1,8 @@
 using ConstructionEquipmentRental.API.Middlewares;
 using IntelliPM.Data.Contexts;
 using IntelliPM.Repositories.AccountRepos;
+using IntelliPM.Repositories.DocumentRepos;
+using IntelliPM.Repositories.DocumentRepos.DocumentRepository;
 using IntelliPM.Repositories.DynamicCategoryRepos;
 using IntelliPM.Repositories.EpicRepos;
 using IntelliPM.Repositories.MilestoneRepos;
@@ -9,11 +11,13 @@ using IntelliPM.Repositories.ProjectRepos;
 using IntelliPM.Repositories.RefreshTokenRepos;
 using IntelliPM.Repositories.RequirementRepos;
 using IntelliPM.Repositories.SystemConfigurationRepos;
+using IntelliPM.Repositories.TaskCheckListRepos;
 using IntelliPM.Repositories.TaskRepos;
 using IntelliPM.Services.AccountServices;
 using IntelliPM.Services.AdminServices;
 using IntelliPM.Services.AuthenticationServices;
 using IntelliPM.Services.CloudinaryStorageServices;
+using IntelliPM.Services.DocumentServices;
 using IntelliPM.Services.DynamicCategoryServices;
 using IntelliPM.Services.EmailServices;
 using IntelliPM.Services.EpicServices;
@@ -26,6 +30,7 @@ using IntelliPM.Services.ProjectMemberServices;
 using IntelliPM.Services.ProjectServices;
 using IntelliPM.Services.RequirementServices;
 using IntelliPM.Services.SystemConfigurationServices;
+using IntelliPM.Services.TaskCheckListServices;
 using IntelliPM.Services.TaskServices;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -56,6 +61,9 @@ builder.Services.AddScoped<IMilestoneRepository, MilestoneRepository>();
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<IProjectMemberRepository, ProjectMemberRepository>();
 builder.Services.AddScoped<IRequirementRepository, RequirementRepository>();
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+builder.Services.AddScoped<ITaskCheckListRepository, TaskCheckListRepository>();
+
 
 
 
@@ -76,6 +84,9 @@ builder.Services.AddScoped<IMilestoneService, MilestoneService>();
 builder.Services.AddScoped<ITaskService, TaskService>();
 builder.Services.AddScoped<IProjectMemberService, ProjectMemberService>();
 builder.Services.AddScoped<IRequirementService, RequirementService>();
+builder.Services.AddScoped<ITaskCheckListService, TaskCheckListService>();
+
+builder.Services.AddScoped<IDocumentService, DocumentService>();
 
 
 
