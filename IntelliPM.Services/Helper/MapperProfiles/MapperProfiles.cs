43c933ae042a8bd7eeccde8f1ed20295fa43d696
thaovy305 <ngothaovy305@gmail.com>
2025-06-16 19:12:51 +0700

    merge

--- IntelliPM.Services/Helper/MapperProfiles/MapperProfiles.cs
+++ IntelliPM.Services/Helper/MapperProfiles/MapperProfiles.cs
@@ -63,8 +63,7 @@ namespace IntelliPM.Services.Helper.MapperProfiles
                 .ForMember(dest => dest.StartDate, opt => opt.MapFrom(src => src.StartDate))
                 .ForMember(dest => dest.EndDate, opt => opt.MapFrom(src => src.EndDate));
             CreateMap<Epic, EpicResponseDTO>();
-            //
-
+            
             // Sprint
             CreateMap<SprintRequestDTO, Sprint>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore())
