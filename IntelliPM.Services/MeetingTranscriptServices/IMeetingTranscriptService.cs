3f8f9ec8cc663d8eefddedbacfd7d93c0c9d1c60
khangper <89836419+khangper@users.noreply.github.com>
2025-06-17 15:09:27 +0700

    meetingTranscriptService and meetingLog

---
+++ IntelliPM.Services/MeetingTranscriptServices/IMeetingTranscriptService.cs
@@ -0,0 +1,15 @@
+﻿using IntelliPM.Data.DTOs.MeetingTranscript.Request;
+using IntelliPM.Data.DTOs.MeetingTranscript.Response;
+using IntelliPM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IMeetingTranscriptService
+{
+    Task<MeetingTranscriptResponseDTO> UploadTranscriptAsync(MeetingTranscriptRequestDTO dto);
+    Task<MeetingTranscriptResponseDTO> GetTranscriptByMeetingIdAsync(int meetingId);
+}
+
