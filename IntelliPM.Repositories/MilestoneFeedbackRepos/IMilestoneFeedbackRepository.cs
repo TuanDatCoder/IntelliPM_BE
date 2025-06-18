28bea504500d3639b29bd503dfc441e8090cb8b6
khangper <89836419+khangper@users.noreply.github.com>
2025-06-18 14:00:27 +0700

    MilestoneFeedback

---
+++ IntelliPM.Repositories/MilestoneFeedbackRepos/IMilestoneFeedbackRepository.cs
@@ -0,0 +1,9 @@
+﻿using IntelliPM.Data.Entities;
+
+namespace IntelliPM.Repositories.MilestoneFeedbackRepos
+{
+    public interface IMilestoneFeedbackRepository
+    {
+        Task AddAsync(MilestoneFeedback feedback);
+    }
+}
