fba8a507154c2610318abbb259263e0720c80948
TuanDatCoder <tuandatdq03@gmail.com>
2025-07-07 21:32:48 +0700

    feat api create epic task

---
+++ IntelliPM.Data/DTOs/Epic/Request/EpicTaskAssignedMembersRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Data.DTOs.Epic.Request
+{
+    public class EpicTaskAssignedMembersRequestDTO
+    {
+        public string Title { get; set; } 
+        public string Description { get; set; } 
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string SuggestedRole { get; set; } 
+        public List<TaskAssignedMembersRequestDTO> AssignedMembers { get; set; } = new List<TaskAssignedMembersRequestDTO>();
+    }
+}
