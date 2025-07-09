2c41072eafb0bd5009db051237184a58cb22b15d
hantn127 <hanntn072003@gmail.com>
2025-07-10 01:03:58 +0700

    Add task dependency

---
+++ IntelliPM.Repositories/TaskDependencyRepos/ITaskDependencyRepository.cs
@@ -0,0 +1,15 @@
+﻿using IntelliPM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Repositories.TaskDependencyRepos
+{
+    public interface ITaskDependencyRepository
+    {
+        Task<List<TaskDependency>> GetByProjectIdAsync(int projectId);
+    }
+
+}
