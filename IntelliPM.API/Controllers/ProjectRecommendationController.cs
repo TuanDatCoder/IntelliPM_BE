4973367e2aad2f7439f5846a3f76268bf704e760
hantn127 <hanntn072003@gmail.com>
2025-07-11 14:48:52 +0700

    view suggested recommendation from AI

---
+++ IntelliPM.API/Controllers/ProjectRecommendationController.cs
@@ -0,0 +1,47 @@
+﻿using IntelliPM.Data.DTOs;
+using IntelliPM.Services.ProjectRecommendationServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntelliPM.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProjectRecommendationController : ControllerBase
+    {
+        private readonly IProjectRecommendationService _service;
+
+        public ProjectRecommendationController(IProjectRecommendationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("ai-recommendations")]
+        public async Task<IActionResult> GetAIRecommendations([FromQuery] string projectKey)
+        {
+            try
+            {
+                var result = await _service.GenerateProjectRecommendationsAsync(projectKey);
+                return Ok(new ApiResponseDTO
+                {
+                    IsSuccess = true,
+                    Code = 200,
+                    Message = "Success",
+                    Data = result
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new ApiResponseDTO
+                {
+                    IsSuccess = false,
+                    Code = 500,
+                    Message = $"Internal Server Error: {ex.Message}",
+                    Data = null
+                });
+            }
+        }
+
+    }
+}
---
+++ IntelliPM.Services/ProjectRecommendationServices/IProjectRecommendationService.cs
@@ -0,0 +1,14 @@
+﻿using IntelliPM.Data.DTOs.ProjectRecommendation.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Services.ProjectRecommendationServices
+{
+    public interface IProjectRecommendationService
+    {
+        Task<List<AIRecommendationDTO>> GenerateProjectRecommendationsAsync(string projectKey);
+    }
+}
