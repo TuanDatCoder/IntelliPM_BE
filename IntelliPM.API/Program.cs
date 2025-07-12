f89763c344e6f300fa53d12a5c555bc4b3011497
hantn127 <hanntn072003@gmail.com>
2025-07-12 15:14:23 +0700

    Add ChatGPT Service

--- IntelliPM.API/Program.cs
+++ IntelliPM.API/Program.cs
@@ -82,6 +82,7 @@ using IntelliPM.Services.MeetingRescheduleRequestServices;
 using IntelliPM.Repositories.RiskSolutionRepos;
 using IntelliPM.Repositories.TaskDependencyRepos;
 using IntelliPM.Services.ProjectRecommendationServices;
+using IntelliPM.Services.ChatGPTServices;
 
 
 
@@ -188,6 +189,7 @@ builder.Services.AddHttpClient<ITaskPlanningService, TaskPlanningService>(client
     client.BaseAddress = new Uri("https://generativelanguage.googleapis.com/v1beta/");
 });
 builder.Services.AddHttpClient<IGeminiService, GeminiService>();
+builder.Services.AddHttpClient<IChatGPTService, ChatGPTService>();
 
 
 
