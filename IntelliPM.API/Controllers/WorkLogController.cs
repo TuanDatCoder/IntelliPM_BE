fcede4b68dd9f9424e099c2b36264e88dc2e53b8
hantn127 <hanntn072003@gmail.com>
2025-07-17 10:23:51 +0700

    Add workLog to check actual hours

---
+++ IntelliPM.API/Controllers/WorkLogController.cs
@@ -0,0 +1,25 @@
+﻿using IntelliPM.Services.TaskServices;
+using IntelliPM.Services.WorkLogServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntelliPM.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkLogController : ControllerBase
+    {
+        private readonly IWorkLogService _service;
+
+        public WorkLogController(IWorkLogService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("worklog/daily-generate")]
+        public async Task<IActionResult> GenerateWorkLogs()
+        {
+            await _service.GenerateDailyWorkLogsAsync();
+            return Ok("Work logs generated");
+        }
+    }
+}
---
+++ IntelliPM.Repositories/WorkLogRepos/IWorkLogRepository.cs
@@ -0,0 +1,16 @@
+﻿using IntelliPM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Repositories.WorkLogRepos
+{
+    public interface IWorkLogRepository
+    {
+        Task<bool> ExistsAsync(string? taskId, string? subtaskId, DateTime logDate);
+        Task BulkInsertAsync(List<WorkLog> logs);
+
+    }
+}
