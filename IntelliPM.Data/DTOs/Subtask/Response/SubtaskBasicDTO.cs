48c98b62de412a85b2abb483ec2775b14f9b6f2a
hantn127 <hanntn072003@gmail.com>
2025-07-19 16:00:46 +0700

    Add all api time tracking for tasks

---
+++ IntelliPM.Data/DTOs/Subtask/Response/SubtaskBasicDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Data.DTOs.Subtask.Response
+{
+    public class SubtaskBasicDTO
+    {
+        public string Id { get; set; } = null!;
+        public string TaskId { get; set; } = null!;
+        public int AssignedBy { get; set; }
+        public decimal? PlannedHours { get; set; }
+        public decimal? ActualHours { get; set; }
+    }
+}
