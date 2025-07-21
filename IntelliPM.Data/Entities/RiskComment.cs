f3869d4134489d1260715c091af8678bc9f4f635
hantn127 <hanntn072003@gmail.com>
2025-07-21 12:23:50 +0700

    Update DB - table risk

---
+++ IntelliPM.Data/Entities/RiskComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntelliPM.Data.Entities;
+
+public partial class RiskComment
+{
+    public int Id { get; set; }
+
+    public int RiskId { get; set; }
+
+    public int AccountId { get; set; }
+
+    public string Comment { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual Risk Risk { get; set; } = null!;
+}
---
+++ IntelliPM.Data/Entities/RiskFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntelliPM.Data.Entities;
+
+public partial class RiskFile
+{
+    public int Id { get; set; }
+
+    public int RiskId { get; set; }
+
+    public string FileName { get; set; } = null!;
+
+    public string FileUrl { get; set; } = null!;
+
+    public int UploadedBy { get; set; }
+
+    public DateTime UploadedAt { get; set; }
+
+    public virtual Risk Risk { get; set; } = null!;
+}
