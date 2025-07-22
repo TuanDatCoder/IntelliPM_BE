41daafda4fa0edfdff40fdf6248e946c0d684b31
duyluongg <duyluongg1910@gmail.com>
2025-07-22 23:45:03 +0700

    notify when tagged in document

---
+++ IntelliPM.API/Controllers/NotificationController.cs
@@ -0,0 +1,28 @@
+﻿using IntelliPM.Services.NotificationServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController : ControllerBase
+{
+    private readonly INotificationService _notificationService;
+
+    public NotificationsController(INotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    [HttpGet("me")]
+    public async Task<IActionResult> GetMyNotifications()
+    {
+        var accountIdClaim = User.FindFirst("accountId")?.Value;
+
+        if (string.IsNullOrEmpty(accountIdClaim) || !int.TryParse(accountIdClaim, out var userId))
+            return Unauthorized();
+
+        var notifications = await _notificationService.GetNotificationsByUserId(userId);
+        return Ok(notifications);
+    }
+}
---
+++ IntelliPM.Services/Notification/INotificationService.cs
@@ -0,0 +1,16 @@
+﻿using IntelliPM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Services.NotificationServices
+{
+    public interface INotificationService
+    {
+        Task SendMentionNotification(List<int> mentionedUserIds, int documentId, string documentTitle, int createdBy);
+
+        Task<List<Notification>> GetNotificationsByUserId(int userId);
+    }
+}
