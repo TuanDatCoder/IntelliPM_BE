8f305ec54c935a1285cdc0c0fd6ef4053b1969a7
hantn127 <hanntn072003@gmail.com>
2025-08-05 09:20:52 +0700

    Add view project member with tasks

---
+++ IntelliPM.Data/DTOs/ProjectMember/Response/ProjectMemberWithTasksResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using IntelliPM.Data.DTOs.Task.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Data.DTOs.ProjectMember.Response
+{
+    public class ProjectMemberWithTasksResponseDTO
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string FullName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string AccountPicture {  get; set; } = null!;
+        public decimal? HourlyRate { get; set; }
+        public int? WorkingHoursPerDay { get; set; }
+        public List<TaskBasicDTO> Tasks { get; set; } = new();
+    }
+}
