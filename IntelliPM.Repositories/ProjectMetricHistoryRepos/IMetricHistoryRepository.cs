9ffee5f8549cb9886632c0cede1eaa2debdef0ad
hantn127 <hanntn072003@gmail.com>
2025-08-11 02:45:49 +0700

    Add project metric history

---
+++ IntelliPM.Repositories/ProjectMetricHistoryRepos/IMetricHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using IntelliPM.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Repositories.MetricHistoryRepos
+{
+    public interface IMetricHistoryRepository
+    {
+        Task Add(ProjectMetricHistory metricHistory);
+        Task<List<ProjectMetricHistory>> GetByProjectIdAsync(int projectId);
+    }
+}
---
+++ IntelliPM.Repositories/ProjectMetricHistoryRepos/MetricHistoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using IntelliPM.Data.Contexts;
+using IntelliPM.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliPM.Repositories.MetricHistoryRepos
+{
+    public class MetricHistoryRepository : IMetricHistoryRepository
+    {
+        private readonly Su25Sep490IntelliPmContext _context;
+
+        public MetricHistoryRepository(Su25Sep490IntelliPmContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(ProjectMetricHistory metricHistory)
+        {
+            await _context.ProjectMetricHistory.AddAsync(metricHistory);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<ProjectMetricHistory>> GetByProjectIdAsync(int projectId)
+        {
+            return await _context.ProjectMetricHistory
+                .Where(h => h.ProjectId == projectId)
+                .OrderByDescending(h => h.RecordedAt)
+                .ToListAsync();
+        }
+    }
+}
